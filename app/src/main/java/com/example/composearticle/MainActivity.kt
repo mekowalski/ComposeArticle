1dea592e72f9b7f572000fd856bd7aefb190c885
Malind Kowalski <malind@agsnm.com>
2025-03-18 13:42:40 -0600

    minor modifications and display screenshot

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,11 +58,18 @@ private fun ArticleCard(
     modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier) {
-        Image(painter = imagePainter, contentDescription = null)
+        Image(
+            painter = imagePainter,
+            contentDescription = null,
+            modifier = Modifier.padding(all = 15.dp)
+        )
         Text(
             text = title,
-            modifier = Modifier.padding(15.dp),
-            fontSize = 24.sp
+            //modifier = Modifier.padding(15.dp)
+            modifier = Modifier.fillMaxWidth() .padding(21.dp),
+            fontSize = 24.sp,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.End
         )
         Text(
             text = summary,
